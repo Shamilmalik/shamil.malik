dc7890b146ce19c338bfdafaf6c7d82aa4087303
Shamilmalik <91115034+Shamilmalik@users.noreply.github.com>
2021-09-23 19:18:02 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = <b> f"{file_name} \n. \nJOIN NOW✅️ - https://t.me/joinchat/0B7v3NByJNQ5MGVl", </b>
+                caption = <b> f"{file_name} \n. \nJOIN NOW✅️ - https://t.me/joinchat/0B7v3NByJNQ5MGVl",</b>
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
